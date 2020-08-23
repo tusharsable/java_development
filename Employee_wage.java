d78ac1dec80c1f50c6317e5f65d61f6a9e8abf02
tushar.sable <tushar.sabe@gmail.com>
2020-08-23 23:43:47 +0530

    New program to calculate employee wage

---
+++ Employee_wage.java
@@ -0,0 +1,10 @@
+//Program to calculate salary of a employee
+
+public class Employee_wage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# java_development
-practice problems java
