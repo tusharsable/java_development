1e5538aeb3cd10d01510e1fd08db4e7372e493fa
tushar.sable <tushar.sabe@gmail.com>
2020-08-24 00:19:31 +0530

    calculated day wage for employee

--- Employee_wage.java
+++ Employee_wage.java
@@ -5,16 +5,26 @@ public class Employee_wage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		//declaring required variables
-		int Is_Present = 1;
-
+		int is_present = 1;
+		boolean is_fulltime = true;
+		int worked_hour = 0;
+		int wage_per_hr = 20;
+		int fulltime_hr = 8;
 		//getting random integer number 1 for present and 0 if absent
 		double attendance = Math.floor(Math.random() * 10 ) % 2;
 
 		//checking if employee is present or not
-		if (attendance == Is_Present)
+		if (attendance == is_present) {
 			System.out.println("Employee is Present");
+			worked_hour = fulltime_hr;
+			}
 		else
 			System.out.println("Employee is absent");
+
+		//calculate wage for day
+		int day_wage=worked_hour*wage_per_hr;
+		
+		System.out.println("The day wage is : "+day_wage);
 
 	}
 
