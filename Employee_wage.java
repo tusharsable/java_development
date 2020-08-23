e34df1223138389b60450391360df615f3f908ab
tushar.sable <tushar.sabe@gmail.com>
2020-08-24 00:33:56 +0530

    Added Part time feature

--- Employee_wage.java
+++ Employee_wage.java
@@ -6,17 +6,30 @@ public class Employee_wage {
 
 		//declaring required variables
 		int is_present = 1;
-		boolean is_fulltime = true;
+		int is_full_time = 1;
 		int worked_hour = 0;
 		int wage_per_hr = 20;
-		int fulltime_hr = 8;
+		int full_time_hr = 8;
+		int part_time_hr = 4;
 		//getting random integer number 1 for present and 0 if absent
 		double attendance = Math.floor(Math.random() * 10 ) % 2;
+
+		//getting randon integer number 1 for full_time and 0 for part_time employee
+		double work_type = Math.floor(Math.random() * 10 ) % 2;
 
 		//checking if employee is present or not
 		if (attendance == is_present) {
 			System.out.println("Employee is Present");
-			worked_hour = fulltime_hr;
+
+			if (work_type == is_full_time) {
+				
+				worked_hour = full_time_hr;
+				System.out.println("Full time work done");
+					}
+			else {
+				worked_hour = part_time_hr;
+				System.out.println("Part time work done");
+				}
 			}
 		else
 			System.out.println("Employee is absent");
