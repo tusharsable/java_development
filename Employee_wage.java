f04baa23b18a6a300b965846da8ab46607b371d0
tushar.sable <tushar.sabe@gmail.com>
2020-08-24 00:57:25 +0530

    used switch case instead of if else

--- Employee_wage.java
+++ Employee_wage.java
@@ -5,35 +5,49 @@ public class Employee_wage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		//declaring required variables
-		int is_present = 1;
-		int is_full_time = 1;
 		int worked_hour = 0;
 		int wage_per_hr = 20;
 		int full_time_hr = 8;
 		int part_time_hr = 4;
 		//getting random integer number 1 for present and 0 if absent
-		double attendance = Math.floor(Math.random() * 10 ) % 2;
+		int attendance = (int) Math.floor(Math.random() * 10 ) % 2;
 
 		//getting randon integer number 1 for full_time and 0 for part_time employee
-		double work_type = Math.floor(Math.random() * 10 ) % 2;
+		int work_type = (int) Math.floor(Math.random() * 10 ) % 2;
 
 		//checking if employee is present or not
-		if (attendance == is_present) {
+		switch(attendance)
+		{
+		// if employee present
+		case 1:{
 			System.out.println("Employee is Present");
+			switch(work_type){
 
-			if (work_type == is_full_time) {
-				
-				worked_hour = full_time_hr;
-				System.out.println("Full time work done");
+				//if employee works full time
+				case 1:{
+					worked_hour = full_time_hr;
+					System.out.println("Full time work done");
+				break;
 					}
-			else {
-				worked_hour = part_time_hr;
-				System.out.println("Part time work done");
-				}
-			}
-		else
-			System.out.println("Employee is absent");
 
+				//if employee works part time
+				case 0: {
+					worked_hour = part_time_hr;
+					System.out.println("Part time work done");
+				break;
+					}
+				}
+
+			break;
+
+				}
+		// if employee is absent
+		case 0:
+			{
+				System.out.println("Employee is absent");
+				break;
+			}
+		}
 		//calculate wage for day
 		int day_wage=worked_hour*wage_per_hr;
 		
