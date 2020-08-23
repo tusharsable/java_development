1b92b4778d1da936e2c5b9c639ba5c5c018f1306
tushar.sable <tushar.sabe@gmail.com>
2020-08-24 00:00:28 +0530

    Check if employee present

--- Employee_wage.java
+++ Employee_wage.java
@@ -4,6 +4,17 @@ public class Employee_wage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
+		//declaring required variables
+		int Is_Present = 1;
+
+		//getting random integer number 1 for present and 0 if absent
+		double attendance = Math.floor(Math.random() * 10 ) % 2;
+
+		//checking if employee is present or not
+		if (attendance == Is_Present)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is absent");
 
 	}
 
