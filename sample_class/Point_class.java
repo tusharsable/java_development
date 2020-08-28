89ea27c4718f1881c6c0cecbc2be2deabd07a27e
tushar.sable <tushar.sabe@gmail.com>
2020-08-28 15:32:18 +0530

    added comments

--- sample_class/Point_class.java
+++ sample_class/Point_class.java
@@ -14,7 +14,7 @@ public class Point_class {
 
 	//defining instance methods
 	public float distance_between(Point_class point) {
-
+		//calculating distance between points
 		float distance = (float) Math.sqrt(Math.pow((this.x-point.x),2)+Math.pow((this.y-point.y),2));
 		return distance;
 	}
@@ -22,8 +22,11 @@ public class Point_class {
 	//defining main method
 	public static void main (String[] args) {
 
+		//creating instance variables
 		Point_class point1=new Point_class(2,2);
 		Point_class point2 = new Point_class(9,10);
+
+		//calling instance method
 		float distance = point1.distance_between(point2);
 		System.out.println("distance between points is : "+distance);
 	}
