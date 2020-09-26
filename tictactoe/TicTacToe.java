398ba800059b1a6e6dd0220a308a627d4c49bce9
tushar.sable <tushar.sabe@gmail.com>
2020-09-26 10:26:49 +0530

    Added method to select user key

---
+++ tictactoe/TicTacToe.java
@@ -0,0 +1,36 @@
+//welcome to tictactoe
+import java.util.*;
+
+public class TicTacToe {
+
+    public char[] board =new char[11];
+    public char userChoice;
+    public char compChoice;
+    //creating board UC!
+    TicTacToe() {
+        for(int i=1;i<11;i++) {
+            board[i]=' ';
+        }
+        System.out.println(board);
+    }
+    //UC 2
+    public void UserChoice() {
+        System.out.println("enter 'X' or 'O' as your key ");
+        Scanner sc = new Scanner(System.in);
+        this.userChoice = sc.next().charAt(0);
+        System.out.println("User choice is :- "+userChoice);
+        if (this.userChoice=='X') {
+            this.compChoice='O';
+        }
+        else {
+            this.compChoice='X';
+        }
+        sc.close();
+    }
+    
+    public static void main(String[] args) {
+        TicTacToe newBoard = new TicTacToe();
+        newBoard.UserChoice();
+
+    }
+}
